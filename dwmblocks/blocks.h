c3e6e83ae9d309f12aac9eecc453d0e83cd27042
Giovanni <indirizzoemail@mail.com>
2021-04-17 03:27:09 +0200

    update dwmblocks config in order to show date, ip and vpn connection

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,11 @@
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/* Icon */  /* Command */ 													/* Update Interval */ /* Update Signal */
+	{ "", 		"date '+%H:%M %d-%m-%y'", 										  			10, 	0 },
+	{ "ip: ", 	"/sbin/ip -o -4 addr list eno1 | awk '{print $4}' | cut -d/ -f1", 			120, 	0 },
+	{ "vpn: ", 	"pgrep -x 'openvpn' > /dev/null && echo yes || echo no",					10, 	0 },
+};
+
+// sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
