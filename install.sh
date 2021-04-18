448f2694b8da02e1875025be364e25216feddfc1
Giovanni <indirizzoemail@mail.com>
2021-04-18 18:29:06 +0200

    remove bloated pywal

--- install.sh
+++ install.sh
@@ -23,16 +23,16 @@ make
 sudo make install
 cd ..
 
-cd pywal
-pip3 install --user .
-cd ..
+# cd pywal
+# pip3 install --user .
+# cd ..
 
-wal -i wallpaper --saturate 0.85
+# wal -i wallpaper --saturate 0.85
 
-cp .Xresources .Xresources.wal
-cat $HOME/.cache/wal/colors.Xresources >> .Xresources.wal
+# cp .Xresources .Xresources.wal
+# cat $HOME/.cache/wal/colors.Xresources >> .Xresources.wal
 
-cp .Xresources.wal $HOME/.Xresources
+cp .Xresources $HOME/.Xresources
 xrdb -merge $HOME/.Xresources
 
 cp .bashrc $HOME/
