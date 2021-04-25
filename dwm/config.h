6b9dc1df44015acee6c5bd3a3ad96945d50d6bd0
Giovanni <indirizzoemail@mail.com>
2021-04-25 13:28:14 +0200

    blue

--- dwm/config.h
+++ dwm/config.h
@@ -28,7 +28,7 @@ static char *colors[][3] = {
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
-static char main_xresource_color[] = "color11";
+static char main_xresource_color[] = "color12";
 
 typedef struct {
 	const char *name;
@@ -190,7 +190,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_a,		defaultgaps,	{0} },
 	{ MODKEY,			XK_s,		togglesticky,	{0} },
 	/* { MODKEY|ShiftMask,		XK_s,		spawn,		SHCMD("") }, */
-	{ MODKEY,			XK_d,		spawn,          SHCMD("dmenu_run -nb '#1e1e1e' -sf '#1e1e1e' -sb '#fabd2f' -nf '#fabd2f' ") },
+	{ MODKEY,			XK_d,		spawn,          SHCMD("dmenu_run -nb '#1e1e1e' -sf '#1e1e1e' -sb '#83a598' -nf '#83a598' ") },
 	// { MODKEY|ShiftMask,		XK_d,		spawn,		SHCMD("passmenu") },
 	{ MODKEY,			XK_f,		togglefullscr,	{0} },
 	{ MODKEY|ShiftMask,		XK_f,		setlayout,	{.v = &layouts[8]} },
