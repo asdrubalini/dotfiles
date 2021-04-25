0c92edaac9768bade76315e78e49f8764f250cb8
Giovanni <indirizzoemail@mail.com>
2021-04-26 01:11:21 +0200

    green + docker status in dwmblocks

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -4,6 +4,7 @@ static const Block blocks[] = {
 	{ "", 			"date '+%H:%M %d-%m-%y'", 										  			10, 	0 },
 	{ "ip: ", 		"ip -o -4 addr list eno1 | awk '{print $4}' | cut -d/ -f1", 			120, 	0 },
 	{ "vpn: ", 		"pgrep -x 'openvpn' > /dev/null && echo yes || echo no",					10, 	0 },
+	{ "docker: ", 		"systemctl is-active docker > /dev/null && echo running || echo stopped",					10, 	0 },
 	{ "battery: ", 	"battery | tr -d '\n' && echo %",											60, 	0 },
 };
 
