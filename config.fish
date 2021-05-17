4bea21431cd57b54f7f541240e8480be42b2b67d
Giovanni <indirizzoemail@mail.com>
2021-05-17 21:16:50 +0200

    use fish

---
+++ config.fish
@@ -0,0 +1,11 @@
+alias ls="exa --color=auto"
+
+alias gits="git status"
+alias gita="git add"
+alias gitc="git commit"
+alias gitp="git push"
+
+export LANG=en_US.UTF-8
+export EDITOR=vim
+export BROWSER=google-chrome-stable
+
