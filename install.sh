7d3554de30ef694457e5fec4a88e25384d0668e7
Giovanni <indirizzoemail@mail.com>
2021-07-12 13:10:59 +0200

    nixify install script

--- install.sh
+++ install.sh
@@ -1,23 +1,3 @@
-cd dwm
-make
-doas make install
-cd ..
-
-cd dwmblocks
-make
-doas make install
-cd ..
-
-cd dmenu
-make
-doas make install
-cd ..
-
-cd slock
-make
-doas make install
-cd ..
-
 cp .Xresources $HOME/.Xresources
 xrdb -merge $HOME/.Xresources
 
@@ -26,9 +6,6 @@ cp .xinitrc $HOME/
 
 cp wallpaper $HOME/.wallpaper
 
-cp bin/* $HOME/.local/bin
-chmod +x $HOME/.local/bin/*
-
 mkdir -p $HOME/.config/alacritty/
 cp alacritty/* $HOME/.config/alacritty/
 
@@ -36,6 +13,6 @@ mkdir -p $HOME/.config/picom/
 cp picom/* $HOME/.config/picom/
 
 mkdir -p $HOME/.config/nvim/
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.config/nvim/bundle/Vundle.vim
+[ ! -d $HOME/.config/nvim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.config/nvim/bundle/Vundle.vim
 cp -r nvim/* $HOME/.config/nvim/
 
